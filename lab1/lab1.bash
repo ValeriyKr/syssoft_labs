95d1aa1db46b45f958aca7ec1f7c91ca10deacda
Valeriy Kireev <valeriykireev@gmail.com>
2017-03-12 16:24:39 +0300

    Fix reading
    Fix indents
    
    Fix semicolons

--- lab1/lab1.bash
+++ lab1/lab1.bash
@@ -6,28 +6,18 @@
 #
 
 exec 3>&2 2>>$HOME/lab1_err
-
-
-read_filename() {
-        old_IFS="$IFS"
-        IFS=''
-        read filename
-        IFS="$old_IFS"
-        echo "$filename"
-}
-
-
+IFS=''
 cmd=''
 while {
         # Indents with tabs! (Because of bash)
         cat <<- EOF
-		1. Print working directory
-		2. Print working directory contents
-		3. Create directory
-		4. Add writing permissions to directory
-		5. Take writing permissions from directory
-		6. exit
-		Enter a command:
+				1. Print working directory
+				2. Print working directory contents
+				3. Create directory
+				4. Add writing permissions to directory
+				5. Take writing permissions from directory
+				6. exit
+				Enter a command:
 		EOF
         # Normal indents
         read cmd
@@ -40,33 +30,26 @@ do
                 ;;
         # Commands
         1)
-                pwd -P || echo 'Error: pwd' >&3
-                ;;
+                pwd -P || echo 'Error: pwd' >&3 ;;
         2)
-                ls || echo 'Error: ls' >&3
-                ;;
+                ls || echo 'Error: ls' >&3 ;;
         3)
                 echo 'Enter directory name:'
-                dirname="$(read_filename)"
-                mkdir -- "$dirname" || echo 'Error: mkdir' >&3
-                ;;
+                read dirname
+                mkdir -- "$dirname" || echo 'Error: mkdir' >&3 ;;
         4)
                 echo 'Enter directory name:'
-                dirname="$(read_filename)"
-                chmod ugo+w -- "$dirname" || echo 'Error: chmod +w' >&3
-                ;;
+                read dirname
+                chmod ugo+w -- "$dirname" || echo 'Error: chmod +w' >&3 ;;
         5)
                 echo 'Enter directory name:'
-                dirname="$(read_filename)"
-                chmod ugo-w -- "$dirname" || echo 'Error: chmod -w' >&3
-                ;;
+                read dirname
+                chmod ugo-w -- "$dirname" || echo 'Error: chmod -w' >&3 ;;
         6)
-                break
-                ;;
+                break ;;
         # Anything else
         *)
-                echo 'Incorrect command' >&3
-                ;;
+                echo 'Incorrect command' >&3 ;;
         esac
         echo
 done
