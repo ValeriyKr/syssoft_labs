533f0a4626f3f991ec20e1b6a05819749a3e57ee
Valeriy Kireev <valeriykireev@gmail.com>
2017-05-21 12:41:52 +0300

    Implement task3.2

---
+++ lab5/signalserver.c
@@ -0,0 +1,85 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+#include "state.h"
+
+
+static struct state state;
+
+
+static bool_t update() {
+        time_t current_time;
+        if (-1 == time(&current_time)) {
+                perror("time");
+                _exit(1);
+        }
+        if (current_time - state.start_time > state.working_time) {
+                state.working_time = current_time - state.start_time;
+                if (-1 == getloadavg(state.loadavg, 3)) {
+                        perror("loadavg");
+                        _exit(2);
+                }
+                return true;
+        }
+        return false;
+}
+
+
+static void sighandler(int signo) {
+        signal(signo, sighandler);
+        switch (signo) {
+        case SIGHUP:
+                printf("PID: %d\n", state.pid);
+                break;
+        case SIGINT:
+                printf("UID: %u\n", state.uid);
+                break;
+        case SIGTERM:
+                printf("GID: %u\n", state.gid);
+                break;
+        case SIGUSR1:
+                printf("Working time: %lu\n", state.working_time);
+                break;
+        case SIGUSR2:
+                printf("Load average for 1 minute: %lf\n", state.loadavg[0]);
+                printf("Load average for 5 minutes: %lf\n", state.loadavg[1]);
+                printf("Load average for 15 minutes: %lf\n", state.loadavg[2]);
+        }
+}
+
+
+int main() {
+        int sleep_ret;
+
+        if (-1 == time(&state.start_time)) {
+                perror("time");
+                _exit(1);
+        }
+        state.pid = getpid(); /* "These functions are always successful." */
+        state.uid = getuid();
+        state.gid = getgid();
+
+        signal(SIGHUP, sighandler);
+        signal(SIGINT, sighandler);
+        signal(SIGTERM, sighandler);
+        signal(SIGUSR1, sighandler);
+        signal(SIGUSR2, sighandler);
+
+        while (sleep_ret = usleep(1000000), true) {
+                if (-1 == sleep_ret) {
+                        if (EINTR == errno) goto force_update;
+                        perror("usleep");
+                        _exit(3);
+                }
+force_update:
+                (void) update();
+        }
+}
